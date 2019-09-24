763410c6678933aeb06014ce5780099499e00fca
igaryok <igor.sobko@gmail.com>
2019-09-24 21:55:50 +0300

    Updates

---
+++ precache-manifest.ff1d830435afca42e4faf7e64d755e90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3325659a492d542eb0b19740b346695c",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "c136bc1018fbc462fc9b",
+    "url": "/react_dynamic-list-of-posts/static/css/main.ba7a6ff3.chunk.css"
+  },
+  {
+    "revision": "098f6b9ec967e0523fde",
+    "url": "/react_dynamic-list-of-posts/static/js/2.2dc4b45a.chunk.js"
+  },
+  {
+    "revision": "c136bc1018fbc462fc9b",
+    "url": "/react_dynamic-list-of-posts/static/js/main.0747942e.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
